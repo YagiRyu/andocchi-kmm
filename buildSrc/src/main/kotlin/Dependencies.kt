8745331b3ffe6dd8f988784ea6e0027ab356fa89
YagiRyu <ryutaro822@icloud.com>
2022-07-10 12:53:40 +0900

    add buildSrc module

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,14 @@
+const val androidPlugin = "android"
+const val androidApp = "com.android.application"
+const val androidKotlin = "org.jetbrains.kotlin.android"
+const val multiplatform = "multiplatform"
+const val composePlugin = "org.jetbrains.compose"
+const val cocoapods = "native.cocoapods"
+const val serializationPlugin = "plugin.serialization"
+const val sqldelightPlugin = "com.squareup.sqldelight"
+
+object Versions {
+    const val minSdk = 21
+    const val targetSdk = 32
+    const val compilerSdk = 32
+}
